f07ef95bf89485ef91a1095dea2b9e7789019699
Théo Claudel <62987640+theocdl@users.noreply.github.com>
2022-04-02 00:54:29 +0200

    Add files via upload

---
+++ Dai.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DAI is ERC20
+{
+    constructor()
+    public ERC20("Magic DAI for hackers", "DAI")
+    {
+        _mint(msg.sender, 1000000*10**18);
+    }
+
+    event Withdrawn(address user, uint amount );
+
+    function withdraw()public
+    {
+        _mint(msg.sender, 50*10**18);
+        emit Withdrawn (msg.sender, 50);
+    }
+}
